1fb85b09a10d5f45da856b6a2289383ef3b2a5f3
lllllinalll <polina.romanenko.2005@bk.ru>
2025-01-31 14:06:48 +0700

    РАБОТАЕТ

--- YP3/Program.cs
+++ YP3/Program.cs
@@ -6,62 +6,67 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите команду (1 - Добавить Продукт, 2 - Продать товар, 3 - Отчет по продажам):");
-        string name = "";
-        User user = new User { Name = "", QuantityInStock = 0, Price = 0 };
-        int choice = int.Parse(Console.ReadLine());
+        bool exit = true;
+        while (exit) {
+            Console.WriteLine("Введите команду (1 - Добавить Продукт, 2 - Продать товар, 3 - Отчет по продажам 4 - Выход):");
+            string name = "";
+            User user = new User { Name = "", QuantityInStock = 0, Price = 0 };
+            int choice = int.Parse(Console.ReadLine());
 
-        switch (choice)
-        {
-            case 1:
-                Console.WriteLine("Напишите Продукт:");
-                name = Console.ReadLine();
-                user = SearchUser(name);
-                if (user == null)
-                {
-                    user = new User { Name = "", QuantityInStock = 0, Price = 0 };
-                    user.Name = name;
-
-                    Console.WriteLine("Введите количество товара:");
-                    user.QuantityInStock = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Введите цену товара:");
-                    user.Price = int.Parse(Console.ReadLine());
-                    CreateUser(user);
-                }
-                else
-                {
-                    Console.WriteLine("Продукт с таким именем уже существует.");
-                }
-                break;
-
-            case 2:
-                Console.WriteLine("Напишите Продукт:");
-                name = Console.ReadLine();
-                user = SearchUser(name);
-                if (user != null)
-                {
-                    Console.WriteLine("Введите количество проданного товара:");
-                    int quantitySold = int.Parse(Console.ReadLine());
-                    while (user.QuantityInStock < quantitySold)
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Напишите Продукт:");
+                    name = Console.ReadLine();
+                    user = SearchUser(name);
+                    if (user == null)
                     {
-                        Console.WriteLine("Продаж должно быть меньше чем на складе. Введите количество снова:");
-                        quantitySold = int.Parse(Console.ReadLine());
+                        user = new User { Name = "", QuantityInStock = 0, Price = 0 };
+                        user.Name = name;
+
+                        Console.WriteLine("Введите количество товара:");
+                        user.QuantityInStock = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите цену товара:");
+                        user.Price = float.Parse(Console.ReadLine());
+                        CreateUser(user);
                     }
-                    user.QuantityInStock -= quantitySold;
-                    UpdateUserInDatabase(user);
-
-                    // Запрос имени покупателя
-                    Console.WriteLine("Введите имя покупателя:");
-                    string buyerName = Console.ReadLine();
-
-                    // Запрос даты продажи у пользователя
-                    Console.WriteLine("Введите дату продажи (в формате ГГГГ-ММ-ДД):");
-                    string dateInput = Console.ReadLine();
-                    DateTime saleDate;
-
-                    // Попробуем преобразовать введённую строку в DateTime
-                    if (DateTime.TryParse(dateInput, out saleDate))
+                    else
                     {
+                        Console.WriteLine("Продукт с таким именем уже существует.");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Напишите Продукт:");
+                    name = Console.ReadLine();
+                    user = SearchUser(name);
+                    if (user != null)
+                    {
+                        Console.WriteLine("Введите количество проданного товара:");
+                        int quantitySold = int.Parse(Console.ReadLine());
+                        while (user.QuantityInStock < quantitySold)
+                        {
+                            Console.WriteLine("Продаж должно быть меньше чем на складе. Введите количество снова:");
+                            quantitySold = int.Parse(Console.ReadLine());
+                        }
+                        user.QuantityInStock -= quantitySold;
+                        UpdateUserInDatabase(user);
+
+                        // Запрос имени покупателя
+                        Console.WriteLine("Введите имя покупателя:");
+                        string buyerName = Console.ReadLine();
+
+                        // Запрос даты продажи у пользователя
+                        Console.WriteLine("Введите дату продажи (в формате ГГГГ-ММ-ДД):");
+                        string dateInput = Console.ReadLine();
+                        DateTime saleDate;
+                        while (!DateTime.TryParse(dateInput, out saleDate))
+                        {
+                            // Преобразуем в UTC, если дата не в UTC
+                            Console.WriteLine("Неверный формат даты. Отчет не был сформирован.");
+                            dateInput = Console.ReadLine();
+                        }
+                        // Попробуем преобразовать введённую строку в DateTime
                         // Преобразуем в UTC, если дата не в UTC
                         if (saleDate.Kind != DateTimeKind.Utc)
                         {
@@ -69,7 +74,7 @@ class Program
                         }
 
                         // Рассчитываем сумму продажи
-                        int totalSaleAmount = quantitySold * user.Price;
+                        float totalSaleAmount = quantitySold * user.Price;
 
                         // Создание новой записи о продаже
                         CreateSale(new Sale
@@ -84,66 +89,58 @@ class Program
                         // Выводим сумму продажи на экран
                         Console.WriteLine($"Сумма продажи: {totalSaleAmount} (Количество: {quantitySold}, Цена за единицу: {user.Price})");
                     }
+
                     else
                     {
-                        Console.WriteLine("Неверный формат даты. Продажа не была зарегистрирована.");
+                        Console.WriteLine("Продукт не найден.");
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Продукт не найден.");
-                }
-                break;
+                    break;
 
-            case 3:
-                Console.WriteLine("ОТЧЕТ ПО ПРОДАЖАМ:\n");
-                Console.WriteLine("Введите дату начала периода (в формате ГГГГ-ММ-ДД):");
-                string startDateInput = Console.ReadLine();
-                DateTime startDate;
+                case 3:
+                    Console.WriteLine("ОТЧЕТ ПО ПРОДАЖАМ:\n");
+                    Console.WriteLine("Введите дату начала периода (в формате ГГГГ-ММ-ДД):");
+                    string startDateInput = Console.ReadLine();
+                    DateTime startDate;
 
-                // Попробуем преобразовать введённую строку в DateTime
-                if (DateTime.TryParse(startDateInput, out startDate))
-                {
-                    // Преобразуем в UTC, если дата не в UTC
+
+                    while (!DateTime.TryParse(startDateInput, out startDate))
+                    {
+                        // Преобразуем в UTC, если дата не в UTC
+                        Console.WriteLine("Неверный формат даты. Отчет не был сформирован.");
+                        startDateInput = Console.ReadLine();
+                    }
                     if (startDate.Kind != DateTimeKind.Utc)
                     {
                         startDate = TimeZoneInfo.ConvertTimeToUtc(startDate);
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Неверный формат даты. Отчет не был сформирован.");
-                    break;
-                }
+                    Console.WriteLine("Введите дату конца периода (в формате ГГГГ-ММ-ДД):");
+                    string endDateInput = Console.ReadLine();
+                    DateTime endDate;
 
-                Console.WriteLine("Введите дату конца периода (в формате ГГГГ-ММ-ДД):");
-                string endDateInput = Console.ReadLine();
-                DateTime endDate;
 
-                // Попробуем преобразовать введённую строку в DateTime
-                if (DateTime.TryParse(endDateInput, out endDate))
-                {
-                    // Преобразуем в UTC, если дата не в UTC
+                    while (!DateTime.TryParse(endDateInput, out endDate))
+                    {
+                        // Преобразуем в UTC, если дата не в UTC
+                        Console.WriteLine("Неверный формат даты. Отчет не был сформирован.");
+                        endDateInput = Console.ReadLine();
+                    }
                     if (endDate.Kind != DateTimeKind.Utc)
                     {
                         endDate = TimeZoneInfo.ConvertTimeToUtc(endDate);
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Неверный формат даты. Отчет не был сформирован.");
-                    break;
-                }
 
-                using (ApplicationContext db = new ApplicationContext())
-                {
-                    var sales = db.Shops.Where(s => s.SaleDate >= startDate && s.SaleDate <= endDate).ToList();
-                    foreach (var sale in sales)
+                    using (ApplicationContext db = new ApplicationContext())
                     {
-                        Console.WriteLine($"Пользователь: {sale.UserName}, Имя покупателя: {sale.BuyerName}, Количество: {sale.QuantitySold}, Сумма: {sale.Sell}, Дата: {sale.SaleDate}");
+                        var sales = db.Shops.Where(s => s.SaleDate >= startDate && s.SaleDate <= endDate).ToList();
+                        foreach (var sale in sales)
+                        {
+                            Console.WriteLine($"Продукт: {sale.UserName}, Имя покупателя: {sale.BuyerName}, Количество: {sale.QuantitySold}, Сумма: {sale.Sell}, Дата: {sale.SaleDate}");
+                        }
                     }
-                }
-                break;
+                    break;
+                case 4:
+                        exit = false; break;
+            }
         }
     }
 
@@ -186,8 +183,8 @@ class Program
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public int QuantityInStock { get; set; } // 'much'
-        public int Price { get; set; }
+        public float QuantityInStock { get; set; } // 'much'
+        public float Price { get; set; }
     }
 
     public class Sale
@@ -195,9 +192,9 @@ class Program
         public int Id { get; set; }
         public string UserName { get; set; } // 'name'
         public string BuyerName { get; set; } // Новое поле для имени покупателя
-        public int QuantitySold { get; set; } // 'sell'
+        public float QuantitySold { get; set; } // 'sell'
         public DateTime SaleDate { get; set; } // 'date'
-        public int Sell { get; set; } // Поле для хранения суммы продажи
+        public float Sell { get; set; } // Поле для хранения суммы продажи
     }
 
     public class ApplicationContext : DbContext
